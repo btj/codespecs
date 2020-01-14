59b593930ffde74b9ff4b08ea7cfd39aa8282576
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-01-14 17:26:23 +0100

    Improve exception messages

--- codespecs/src/codespecs/CodeSpecs.java
+++ codespecs/src/codespecs/CodeSpecs.java
@@ -7,20 +7,20 @@ import java.util.function.Predicate;
 public class CodeSpecs {
 	public static void requires(boolean condition) {
 		if (!condition)
-			throw new PreconditionFailureException("Precondition failure");
+			throw new PreconditionFailureException("Precondition does not hold");
 	}
 	
 	public static <T> Consumer<T> ensures(Predicate<T> condition) {
 		return self -> {
 			if (!condition.test(self))
-				throw new PostconditionFailureException("Postcondition failure");
+				throw new PostconditionFailureException("Postcondition does not hold");
 		};
 	}
 	
 	public static Runnable ensures(BooleanSupplier condition) {
 		return () -> {
 			if (!condition.getAsBoolean())
-				throw new PostconditionFailureException("Postcondition failure");
+				throw new PostconditionFailureException("Postcondition does not hold");
 		};
 	}
 }
